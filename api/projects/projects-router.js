91317debbd7090c26a8de53bffea70088ebd3d3a
kristapants <peachykeen@gmail.com>
2020-12-04 12:53:18 -0600

    middleware and projects done, first test

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const Projects = require('./projects-model');
-const Actions = require('../actions/actions-model')
 
 const router = express.Router();
 
@@ -45,10 +44,51 @@ router.post('/', validateProjectContents, (req, res) => {
         })
 })
 //update an existing project
+router.put('/:id', validateProjectId, validateProjectContents, (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "The project could not be updated."
+            })
+        })
+
+})
+
 
 //delete a project
 
+router.delete('/:id', validateProjectId, (req, res) => {
+    Projects.remove(req.params.id)
+        .then(() => {
+            res.status(201).json({
+                message: "Delete Success"
+            })
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "The project could not be updated."
+            })
+        })
+
+})
+
 //get all actions associated with a project
+
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    Projects.getProjectActions(req.params.id)
+        .then(actions => {
+            res.status(201).json(actions)
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "Could not be completed"
+            })
+        })
+
+})
 
 
 //Middleware
